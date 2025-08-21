0de557ff3b88eb2a0080d9a90e0d4fcb8f870f95
seokjoon27 <seokjoon27@gmail.com>
2025-08-21 21:52:51 +0800

    Level-0

---
+++ src/main/java/ESTJ.java
@@ -0,0 +1,11 @@
+public class ESTJ {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + " Hello! I'm ESTJ\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(logo);
+    }
+}
