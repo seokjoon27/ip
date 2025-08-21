f2830b9055369b27445a6596451e1bf15016f089
seokjoon27 <seokjoon27@gmail.com>
2025-08-22 02:21:31 +0800

    Level-2

--- src/main/java/ESTJ.java
+++ src/main/java/ESTJ.java
@@ -9,6 +9,9 @@ public class ESTJ {
         System.out.println("     What can I do for you?");
         System.out.println(bar);
 
+        String[] tasks = new String[100];
+        int count = 0;
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String userStr = scanner.nextLine().trim();
@@ -18,11 +21,19 @@ public class ESTJ {
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println(bar);
                 break;
+            } else if (userStr.equals(("list"))) {
+                System.out.println(bar);
+                for (int i = 0; i < count; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(bar);
+            } else if (!userStr.isEmpty()) {
+                tasks[count] = userStr;
+                count++;
+                System.out.println(bar);
+                System.out.println("     added: " +userStr);
+                System.out.println(bar);
             }
-
-            System.out.println(bar);
-            System.out.println("     " + userStr);
-            System.out.println(bar);
         }
         scanner.close();
     }
