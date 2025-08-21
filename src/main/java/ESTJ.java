28ca618ff8926b918f20a118e07c58c74d7e76f0
seokjoon27 <seokjoon27@gmail.com>
2025-08-22 02:48:04 +0800

    Level-3

--- src/main/java/ESTJ.java
+++ src/main/java/ESTJ.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 
 public class ESTJ {
+    private static final String bar = "  ____________________________________________________________";
+
     public static void main(String[] args) {
-        String bar = "  ____________________________________________________________";
         System.out.println(bar);
         System.out.println("     Hello! I'm ESTJ");
         System.out.println("     What can I do for you?");
         System.out.println(bar);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -23,12 +24,42 @@ public class ESTJ {
                 break;
             } else if (userStr.equals(("list"))) {
                 System.out.println(bar);
-                for (int i = 0; i < count; i++) {
-                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                if (count == 0) {
+                    System.out.println("     Here are the tasks in your list:");
+                } else {
+                    System.out.println("     Here are the tasks in your list:");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println("     " + (i + 1) + ".[" + tasks[i].getIsDone() + "] " + tasks[i].getTask());
+                    }
                 }
                 System.out.println(bar);
+            } else if (userStr.startsWith("mark ")) {
+                Integer idx = getIndex(userStr.substring(5));
+                if (idx == null || idx < 1 || idx > count) {
+                    break;
+                } else {
+                    Task t = tasks[idx - 1];
+                    t.markDone();
+                    System.out.println(bar);
+                    System.out.println("     Nice! I've marked this task as done:");
+                    System.out.println("       " + t);
+                    System.out.println(bar);
+                }
+            } else if (userStr.startsWith("unmark ")) {
+                Integer idx = getIndex(userStr.substring(7));
+                if (idx == null || idx < 1 || idx > count) {
+                    break;
+                } else {
+                    Task t = tasks[idx - 1];
+                    t.markNotDone();
+                    System.out.println(bar);
+                    System.out.println("     OK, I've marked this task as not done yet:");
+                    System.out.println("       " + t);
+                    System.out.println(bar);
+                }
+
             } else if (!userStr.isEmpty()) {
-                tasks[count] = userStr;
+                tasks[count] = new Task(userStr);
                 count++;
                 System.out.println(bar);
                 System.out.println("     added: " +userStr);
@@ -36,5 +67,44 @@ public class ESTJ {
             }
         }
         scanner.close();
+    }
+
+    private static Integer getIndex(String s) {
+        try {
+            return Integer.parseInt(s.trim());
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+}
+
+class Task {
+    private final String tsk;
+    private boolean isDone;
+
+    public Task(String tsk) {
+        this.tsk = tsk;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    public String getIsDone() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getTask() {
+        return tsk;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getIsDone() + "] " + tsk;
     }
 }
