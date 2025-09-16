50fe5d896a9d745de6ba6b395976d3b89efb718e
seokjoon27 <seokjoon27@gmail.com>
2025-09-17 03:59:09 +0800

    ci(text-ui-test): compile GUI sources with JavaFX modules on *nix & Windows

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,21 +1,29 @@
-@ECHO OFF
+@echo off
+setlocal enabledelayedexpansion
+set ROOT_DIR=%~dp0
+set SRC_DIR=%ROOT_DIR%\src\main\java
+set OUT_DIR=%ROOT_DIR%\out
 
-REM create bin directory if it doesn't exist
-if not exist ..\bin mkdir ..\bin
+if exist "%OUT_DIR%" rmdir /S /Q "%OUT_DIR%"
+mkdir "%OUT_DIR%"
 
-REM delete output from previous run
-if exist ACTUAL.TXT del ACTUAL.TXT
+set FX_LIB=%JAVA_HOME%\lib
+set FX_MODULES=javafx.controls,javafx.fxml
 
-REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
-IF ERRORLEVEL 1 (
-    echo ********** BUILD FAILURE **********
-    exit /b 1
+dir /b "%FX_LIB%\javafx*.jar" >nul 2>&1
+if %errorlevel%==0 (
+  echo [runtest] JavaFX detected at %FX_LIB% -> compiling ALL sources with modules
+  dir /s /b "%SRC_DIR%\*.java" > "%ROOT_DIR%\.sources.txt"
+  javac --module-path "%FX_LIB%" --add-modules %FX_MODULES% -Xlint:none -encoding UTF-8 -d "%OUT_DIR%" @"%ROOT_DIR%\.sources.txt"
+) else (
+  echo [runtest] JavaFX NOT found -> compiling HEADLESS core only (exclude GUI classes)
+  > "%ROOT_DIR%\.sources.txt" (
+    for /f "delims=" %%f in ('dir /s /b "%SRC_DIR%\*.java"') do (
+      echo %%~nxf | findstr /i /x "Main.java Launcher.java MainWindow.java DialogBox.java GuiUi.java" >nul || echo %%f
+    )
+  )
+  javac -Xlint:none -encoding UTF-8 -d "%OUT_DIR%" @"%ROOT_DIR%\.sources.txt"
 )
-REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
-
-REM compare the output to the expected output
-FC ACTUAL.TXT EXPECTED.TXT
+echo [runtest] Compile step done.
+endlocal
